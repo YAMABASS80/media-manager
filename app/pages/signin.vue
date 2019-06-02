257df45dd99ee678cca3afd76ffb101e6606b7e8
Yamabe <myamabe@88e9fe4e36e7.ant.amazon.com>
2019-06-02 23:17:35 +0900

    Add SingIn page, just shows default AWS Amplify Authenticator screen

---
+++ app/pages/signin.vue
@@ -0,0 +1,14 @@
+<template>
+  <amplify-sign-in></amplify-sign-in>
+</template>
+
+<script>
+// import { Authenticator } from 'aws-amplify-vue'
+
+
+export default {
+  name: "signin",
+  layout: "signin"
+}
+
+</script>
