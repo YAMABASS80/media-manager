2bbd1b832609b4f4c73edbf0836477d25b3a5d16
Yamabe <myamabe@88e9fe4e36e7.ant.amazon.com>
2019-06-05 08:03:31 +0900

    Initial media edit page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,8 +5,8 @@
         v-for="(item, i) in items" :key="i"
         :card_title="item.card_title"
         :card_text="item.card_text"
+        :edit_url="'media/' + item.id"
       >
-
       </ContentCard>
     </v-layout>
   </v-container>
@@ -23,31 +23,38 @@ export default {
       items : [
         {
           card_title: "11111111111111111",
-          card_text : "1"
+          card_text : "1",
+          id: "1983741",
         },
         {
           card_title: "22222222222222222",
-          card_text : "2"
+          card_text : "2",
+          id: "5734251",
         },
         {
           card_title: "33333333333333333",
-          card_text : "3"
+          card_text : "3",
+          id: "4029452",
         },
         {
           card_title: "44444444444444444",
-          card_text : "4"
+          card_text : "4",
+          id: "7908806",
         },
         {
           card_title: "55555555555555555",
-          card_text : "5"
+          card_text : "5",
+          id: "0834512",
         },
         {
           card_title: "66666666666666666",
-          card_text : "6"
+          card_text : "6",
+          id: "9084532",
         },
         {
           card_title: "77777777777777777",
-          card_text : "7"
+          card_text : "7",
+          id: "8974036",
         },
       ]
     }
---
+++ app/pages/media/_id.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>
+    {{mediaId}}
+  </h1>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      mediaId: this.$route.params.id
+    }
+  }
+}
+
+</script>
